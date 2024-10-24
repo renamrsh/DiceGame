8657d8cdd5ec045dde31824926a04f8442741fbe
renamrsh <irenamarsh06@gmail.com>
2024-10-24 13:11:11 +0200

    Functions completing

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -5,17 +5,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button rzutBtn, resetBtn;
     private TextView first, second, third, fourth, fifth ;
     private TextView wynikTegoLos, wynikGry, liczbaRzutow;
+    private int sumScore =0;
+    private int rollCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,19 +48,55 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    public void rollDice(){
+    public void rollDice() {
+        Random rand = new Random();
+        int[] diceRes = new int[5];
 
+        for (int i = 0; i < 5; i++) {
+            diceRes[i] = rand.nextInt(6) + 1; // Numbers between 1 and 6
+        }
+
+        displayDiceResults(diceRes);
+
+        int rollScore = 0;
+        for(int i=0; i<diceRes.length;i++){
+            rollScore += diceRes[i];
+        }
+
+        updateScore(rollScore);
+        updateRollCount();
     }
-    public void resetGame(){
 
+    public void resetGame() {
+        first.setText("?");
+        second.setText("?");
+        third.setText("?");
+        fourth.setText("?");
+        fifth.setText("?");
+
+        sumScore = 0;
+        rollCount = 0;
+        wynikTegoLos.setText("Wynik tego losowania: 0");
+        wynikGry.setText("Wynik gry: 0");
+        liczbaRzutow.setText("Liczba rzutów: 0");
     }
-    public void updateScore(int newScore){
 
+    public void updateScore(int newScore) {
+        sumScore += newScore;
+        wynikTegoLos.setText("Wynik tego losowania: " + newScore);
+        wynikGry.setText("Wynik gry: " + sumScore);
     }
-    public void updateRollCount(){
 
+    public void updateRollCount() {
+        rollCount++;
+        liczbaRzutow.setText("Liczba rzutów: " + rollCount);
     }
-    public void displayDiceResults(int[] diceResults){
 
+    public void displayDiceResults(int[] diceResults) {
+        first.setText(String.valueOf(diceResults[0]));
+        second.setText(String.valueOf(diceResults[1]));
+        third.setText(String.valueOf(diceResults[2]));
+        fourth.setText(String.valueOf(diceResults[3]));
+        fifth.setText(String.valueOf(diceResults[4]));
     }
 }
